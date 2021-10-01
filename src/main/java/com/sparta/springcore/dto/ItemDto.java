ca34d2f77c6d38ab0980639d634603e66a07e6ef
codenamehee <jhyfordev@gmail.com>
2021-10-01 15:44:13 +0900

    스프링 심화반 1주차 숙제 전

---
+++ src/main/java/com/sparta/springcore/dto/ItemDto.java
@@ -0,0 +1,12 @@
+package com.sparta.springcore.dto;
+
+import lombok.Getter;
+
+
+@Getter
+public class ItemDto {
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+}
